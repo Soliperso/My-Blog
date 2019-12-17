5f260c76e4c935f2cd473042507df534480b3f0b
Ed Chebli <chah762002@yahoo.fr>
2019-12-17 15:08:48 -0800

    First commit

---
+++ src/build/precache-manifest.938058bad395da80a3cf10326d08c2bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8b5ddf8ee1f1c06e58dacef34f0a1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01d1647a5d93b5d9540",
+    "url": "/static/css/main.fd52f4a8.chunk.css"
+  },
+  {
+    "revision": "e3456c03bbaf969b866b",
+    "url": "/static/js/2.4f3e10bd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4f3e10bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a01d1647a5d93b5d9540",
+    "url": "/static/js/main.842d87d9.chunk.js"
+  },
+  {
+    "revision": "2829fc8889d8b91e8c4b",
+    "url": "/static/js/runtime-main.b519a9b9.js"
+  }
+]);
